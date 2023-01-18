a3fa214bb39299a1e24be1509d1106040da471e0
Robertspoon <robertthemcjug@gmail.com>
2023-01-18 08:39:08 -0400

    Initial commit

---
+++ HealthSystemV2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthSystemV2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Health System Version 2");
+
+            Console.ReadKey(true);
+        }
+    }
+}
