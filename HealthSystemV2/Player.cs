b2e0feb8eca1a6dc248413173ce3bfa5d3e67f70
Robertspoon <robertthemcjug@gmail.com>
2023-01-18 09:43:28 -0400

    erh

---
+++ HealthSystemV2/Player.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthSystemV2
+{
+
+    internal class Player
+    {
+        //fields
+        private int health;
+         private int shields;
+         private int lives;
+
+        //method
+
+        public Player()//constructor
+        {
+            health = 100;
+            shields = 100;
+            lives = 3;
+
+        }
+
+        public void ShowHUD()
+        {
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine("Shields: " + shields);
+            Console.WriteLine("Lives: " + lives);
+        }
+
+        public void TakeDamage(int hp)
+        {
+            Console.WriteLine("You are about to take " + hp +  " damage!");
+            if(hp <0)
+            {
+                hp = 0;
+                Console.WriteLine("You silly billy, you cant input negative numbers :p");
+            }
+            shields = shields - hp;
+            if (shields <0)
+            {
+                health += shields;
+                shields = 0;
+            }
+            if (health <0)
+            {
+                health = 0;
+            }
+        }
+
+        public void Heal(int hp)
+        {
+            health = +hp;
+            if (health >100)
+            {
+                health = 0;
+            }
+        }
+        public void RegenShields(int regenShields)
+        {
+            if (regenShields <0)
+            {
+                regenShields = 0;
+                Console.WriteLine("You cant do that you fool! Negative number is a no no :p");
+            }
+            shields += regenShields;
+            if(shields >100)
+            {
+                shields = 100;
+            }
+        }
+
+
+
+    }
+}
--- HealthSystemV2/Program.cs
+++ HealthSystemV2/Program.cs
@@ -10,7 +10,28 @@ namespace HealthSystemV2
     {
         static void Main(string[] args)
         {
+
+            Player player = new Player();
+
             Console.WriteLine("Health System Version 2");
+            Console.WriteLine();
+            player.ShowHUD();
+            Console.WriteLine();
+            player.TakeDamage(105);
+            player.ShowHUD();
+            Console.WriteLine();
+            player.TakeDamage(-1000);
+            player.ShowHUD();
+            Console.WriteLine();
+            player.RegenShields(200);
+            player.ShowHUD();
+            Console.WriteLine();
+            player.RegenShields(-1000);
+            player.ShowHUD();
+            Console.WriteLine();
+            player.Heal(100);
+            player.ShowHUD();
+
 
             Console.ReadKey(true);
         }
